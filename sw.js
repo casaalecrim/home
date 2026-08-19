6ed82b461f87f72c24436ca7cb9aae6c3f990e60
casaalecrim <casa.alecrim.gs@gmail.com>
2026-08-19 17:49:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@ messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(title, { body, icon: './icon-192.png', badge: './favicon-32.png' });
 });
 
-const CACHE_NAME = 'casa-alecrim-shell-v3';
+const CACHE_NAME = 'casa-alecrim-shell-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png', './favicon-32.png'];
 
 self.addEventListener('install', (event) => {
